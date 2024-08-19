6006e0b10def8a41e8ee045ca3d57935cf0fe51d
gerardo <gerardo.martinez.carmona@gmail.com>
2024-08-18 21:07:52 -0500

    SERV

--- app/services/page.js
+++ app/services/page.js
@@ -31,32 +31,32 @@ export default function Services() {
                 </div>
 
                 <div className="border border-2 border-[#9a8a78] p-5">
-                    <h3>Home Interior Design</h3>
+                    <h3 className="p-5 text-[25px] font-bold">Home Interior Design</h3>
                     <p>
                         Our team of experts also specializes in home interior design. We understand that every home is unique, and we work closely with our customers to create a design that reflects their style and personality. From choosing the right colors to selecting the perfect furniture, we will transform your home into a beautiful and functional space that you will love.</p>
                 </div>
                 <div className="border border-2 border-[#9a8a78] p-5">
-                    <h3>Bathroom Remodeling</h3>
+                    <h3 className="p-5 text-[25px] font-bold">Bathroom Remodeling</h3>
                     <p>Our bathroom remodeling services include custom design and installation of bathroom vanities, cabinets, and showers. We work closely with our customers to create a design that fits their style and needs, ensuring that every detail is perfect. Trust us to deliver exceptional quality work and make your bathroom a beautiful and functional space.</p>
                 </div>
             </section>
 
             <secton className="grid grid-cols-4 gap-5 mt-12 w-[90%] mx-auto">
                 <div className="border border-2 border-[#9a8a78] p-5">
-                    <h3>Our Expertise</h3>
+                    <h3 className="p-5 text-[35px] font-bold">Our Expertise</h3>
 
 
                 </div>
                 <div className="border border-2 border-[#9a8a78] p-5">
-                    <h3>Custom Design</h3>
+                    <h3 className="p-5 text-[25px] font-bold">Custom Design</h3>
                     <p>At Magic Wood Work, we offer custom design services to ensure that your project is unique and reflects the customer's style and personality. Our team of experts will work closely with you to create a design that exceeds your expectations.</p>
                 </div>
                 <div className="border border-2 border-[#9a8a78] p-5">
-                    <h3>Quality Workmanship</h3>
+                    <h3 className="p-5 text-[25px] font-bold">Quality Workmanship</h3>
                     <p>We are committed to delivering the highest quality workmanship in every project we undertake. Our team of experts has many years of experience in the market, ensuring that every project is completed with exceptional craftsmanship.</p>
                 </div>
                 <div className="border border-2 border-[#9a8a78] p-5">
-                    <h3>Customer Satisfaction</h3>
+                    <h3 className="p-5 text-[25px] font-bold">Customer Satisfaction</h3>
                     <p>At Magic Wood Work, we value building trust with our customers by always delivering the best quality work. We are committed to providing excellent service and ensuring customer satisfaction. Trust us to make your dreams come true through our exceptional craftsmanship.</p>
 
                 </div>

58bf0451774f166facf5a1c3d4ba97e71aa6a0c3
gerardo <gerardo.martinez.carmona@gmail.com>
2024-08-19 22:35:09 -0500

    hh

--- app/services/page.js
+++ app/services/page.js
@@ -2,17 +2,18 @@ import Image from "next/image";
 
 const Divider = () => (
     <div className="my-8 flex items-center">
-      <div className="flex-grow border-t border-gray-300"></div>
-      <div className="flex-shrink mx-4">
-        <div className="w-4 h-4 bg-[#9a8a78] rotate-45"></div>
-      </div>
-      <div className="flex-grow border-t border-gray-300"></div>
+        <div className="flex-grow border-t border-gray-300"></div>
+        <div className="flex-shrink mx-4">
+            <div className="w-4 h-4 bg-[#9a8a78] rotate-45"></div>
+        </div>
+        <div className="flex-grow border-t border-gray-300"></div>
     </div>
-  );
-  
+);
+
 export default function Services() {
     return (
         <main className="px-4 md:px-0 pt-8">
+            <Divider />
             <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-8 w-full md:w-[90%] mx-auto">
                 <div className="border-2 border-[#9a8a78]  w-full lg:col-span-1">
                     <h2 className="p-4 text-2xl font-bold md:text-3xl lg:text-[35px]">Our Services </h2>
@@ -26,7 +27,7 @@ export default function Services() {
                     <p className="text-xs text-gray-300">One of our specialties is custom kitchen cabinet design and installation. We understand that the kitchen is the heart of the home, and we work closely with our customers to create a design that fits their style and needs.</p>
                 </div>
                 <div className="w-full h-64 md:h-full relative lg:col-span-1">
-                    <Image 
+                    <Image
                         src="/imagenes/servicio1.jpg"
                         alt="services"
                         layout="fill"
@@ -35,10 +36,10 @@ export default function Services() {
                     />
                 </div>
             </section>
-
+            <Divider />
             <section className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-8 w-full md:w-[90%] mx-auto">
                 <div className="w-full h-64 md:h-full relative lg:col-span-1">
-                    <Image 
+                    <Image
                         src="/imagenes/servicio2.jpg"
                         alt="services"
                         layout="fill"
@@ -55,7 +56,7 @@ export default function Services() {
                     <p className="text-xs text-gray-300 ">Our bathroom remodeling services include custom design and installation of bathroom vanities, cabinets, and showers. We work closely with our customers to create a design that fits their style and needs.</p>
                 </div>
             </section>
-
+            <Divider />
             <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-8 w-full md:w-[90%] mx-auto">
                 <div className="border-2 border-[#9a8a78] p-4">
                     <h3 className="p-4 text-2xl font-bold md:text-3xl lg:text-[35px]">Our Expertise</h3>
@@ -73,6 +74,7 @@ export default function Services() {
                     <p className="text-xs text-gray-300 ">At Magic Wood Work, we value building trust with our customers by always delivering the best quality work. We are committed to providing excellent service and ensuring customer satisfaction.</p>
                 </div>
             </section>
+            <Divider />
         </main>
     );
 }

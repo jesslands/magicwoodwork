af62c60e2b1b17b4f1c26dc9d9b4c22f7ae8e3f4
gerardo <gerardo.martinez.carmona@gmail.com>
2024-08-22 21:28:59 -0500

    hgjh

--- app/About/page.js
+++ app/About/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import ContactSection from '/components/contactanos';
 
-export default function About_Us() {
+export default function About() {
     return (
         <main className="px-4 sm:px-6 lg:px-8 pt-8 min-h-screen">
             <ContactSection />
--- components/contactanos.js
+++ components/contactanos.js
@@ -3,66 +3,49 @@ import { FaInstagram, FaEnvelope, FaPhone } from 'react-icons/fa';
 
 const ContactSection = () => {
     return (
-        <section className="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-8 w-full max-w-6xl mx-auto pb-20">
+        <section className="flex flex-col md:flex-row justify-center items-start gap-8 mt-8 w-full max-w-5xl mx-auto pb-20 px-4">
             {/* Formulario */}
-            <div className="lg:col-span-2 p-4 sm:p-6 rounded-lg shadow-lg">
-                <form className="space-y-4 sm:space-y-6">
-                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6">
-                        <div>
-                            <label htmlFor="firstName" className="block text-[#9a8a78] text-sm mb-2">First Name</label>
-                            <input
-                                type="text"
-                                id="firstName"
-                                className="w-full bg-transparent border-b border-gray-600 text-white focus:border-[#9a8a78] focus:outline-none py-2"
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="lastName" className="block text-[#9a8a78] text-sm mb-2">Last Name</label>
-                            <input
-                                type="text"
-                                id="lastName"
-                                className="w-full bg-transparent border-b border-gray-600 text-white focus:border-[#9a8a78] focus:outline-none py-2"
-                            />
-                        </div>
-                    </div>
-
-                    <div>
-                        <label htmlFor="email" className="block text-[#9a8a78] text-sm mb-2">Email*</label>
+            <div className="w-full md:w-1/2 max-w-md p-4 sm:p-6 rounded-lg shadow-lg">
+                <form className="space-y-4">
+                    <div className="grid grid-cols-2 gap-4 ">
+                        
                         <input
-                            type="email"
-                            id="email"
-                            required
+                            type="text"
+                            placeholder="First Name"
+                            className="w-full bg-transparent border-b border-gray-600 text-white focus:border-[#9a8a78] focus:outline-none py-2"
+                        />
+                        <input
+                            type="text"
+                            placeholder="Last Name"
                             className="w-full bg-transparent border-b border-gray-600 text-white focus:border-[#9a8a78] focus:outline-none py-2"
                         />
                     </div>
 
-                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6">
-                        <div>
-                            <label htmlFor="phone" className="block text-[#9a8a78] text-sm mb-2">Phone</label>
-                            <input
-                                type="tel"
-                                id="phone"
-                                className="w-full bg-transparent border-b border-gray-600 text-white focus:border-[#9a8a78] focus:outline-none py-2"
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor="address" className="block text-[#9a8a78] text-sm mb-2">Address</label>
-                            <input
-                                type="text"
-                                id="address"
-                                className="w-full bg-transparent border-b border-gray-600 text-white focus:border-[#9a8a78] focus:outline-none py-2"
-                            />
-                        </div>
+                    <input
+                        type="email"
+                        placeholder="Email*"
+                        required
+                        className="w-full bg-transparent border-b border-gray-600 text-white focus:border-[#9a8a78] focus:outline-none py-2"
+                    />
+
+                    <div className="grid grid-cols-2 gap-4">
+                        <input
+                            type="tel"
+                            placeholder="Phone"
+                            className="w-full bg-transparent border-b border-gray-600 text-white focus:border-[#9a8a78] focus:outline-none py-2"
+                        />
+                        <input
+                            type="text"
+                            placeholder="Address"
+                            className="w-full bg-transparent border-b border-gray-600 text-white focus:border-[#9a8a78] focus:outline-none py-2"
+                        />
                     </div>
 
-                    <div>
-                        <label htmlFor="message" className="block text-[#9a8a78] text-sm mb-2">Message</label>
-                        <textarea
-                            id="message"
-                            rows="4"
-                            className="w-full bg-transparent border border-gray-600 text-white focus:border-[#9a8a78] focus:outline-none p-2 rounded"
-                        ></textarea>
-                    </div>
+                    <textarea
+                        placeholder="Message"
+                        rows="4"
+                        className="w-full bg-transparent border border-gray-600 text-white focus:border-[#9a8a78] focus:outline-none p-2 rounded"
+                    ></textarea>
 
                     <button
                         type="submit"
@@ -74,15 +57,15 @@ const ContactSection = () => {
             </div>
 
             {/* Iconos de contacto */}
-            <div className="flex flex-col justify-center items-center space-y-6 p-4 sm:p-6 rounded-lg shadow-lg">
+            <div className="flex flex-row md:flex-col justify-center items-center space-x-4 md:space-x-0 md:space-y-4 p-4 sm:p-6 rounded-lg shadow-lg">
                 <a href="https://www.instagram.com/tuusuario" target="_blank" rel="noopener noreferrer" className="text-[#9a8a78] hover:text-[#8a7a68] transition duration-300">
-                    <FaInstagram size={40} />
+                    <FaInstagram size={100} />
                 </a>
                 <a href="mailto:tuemail@ejemplo.com" className="text-[#9a8a78] hover:text-[#8a7a68] transition duration-300">
-                    <FaEnvelope size={40} />
+                    <FaEnvelope size={100} />
                 </a>
                 <a href="tel:+1234567890" className="text-[#9a8a78] hover:text-[#8a7a68] transition duration-300">
-                    <FaPhone size={40} />
+                    <FaPhone size={100} />
                 </a>
             </div>
         </section>

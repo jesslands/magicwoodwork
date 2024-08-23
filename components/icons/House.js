d00c9d326011a3a76a1fa0088e6359adbc6ba682
Juan Esteban Espinosa <juanestebanspinosa@gmail.com>
2024-08-23 12:56:25 -0500

    chore: home page completo

--- components/icons/House.js
+++ components/icons/House.js
@@ -1,18 +1,28 @@
-const House = () => (
-    <svg
-        data-bbox="20.005 16.396 160 167.324"
-        viewBox="20.005 16.396 160 167.324"
-        height={50}
-        width={50}
-        xmlns="http://www.w3.org/2000/svg"
-        data-type="color"
-        className="text-[#9A8A78]"
-      >
-        <g>
-          <path fill="#978673" d="M162.524 183.72H37.486V81.18h3.746v98.793h117.545V81.18h3.747v102.54z" data-color="1"/>
-          <path fill="#978673" d="M177.356 99.045L99.995 21.694 22.654 99.045l-2.649-2.649 79.99-80 80.01 80-2.649 2.649z" data-color="1"/>
-        </g>
-      </svg>
+const IconHouse = () => (
+  <svg
+  preserveAspectRatio="xMidYMid meet"
+  data-bbox="20.005 16.396 160 167.324"
+  viewBox="20.005 16.396 160 167.324"
+  height={50}
+  width={50}
+  xmlns="http://www.w3.org/2000/svg"
+  data-type="color"
+  role="presentation"
+  aria-hidden="true"
+>
+  <g>
+    <path
+      fill="#978673"
+      d="M162.524 183.72H37.486V81.18h3.746v98.793h117.545V81.18h3.747v102.54z"
+      data-color="1"
+    />
+    <path
+      fill="#978673"
+      d="M177.356 99.045L99.995 21.694 22.654 99.045l-2.649-2.649 79.99-80 80.01 80-2.649 2.649z"
+      data-color="1"
+    />
+  </g>
+</svg>
 );
 
-export default House;
+export default IconHouse;
--- components/icons/Wall.js
+++ components/icons/Wall.js
@@ -1,23 +1,22 @@
-const Icon = () => (
-    <svg
-    data-bbox="20 20 160 160"
-    viewBox="20 20 160 160"
-    height={50}
-    width={50}
-    xmlns="http://www.w3.org/2000/svg"
-    data-type="color"
-    role="presentation"
-    aria-hidden="true"
-  >
-   
-    <g>
-      <path
-        d="M178 20H22a2 2 0 00-2 2v156a2 2 0 002 2h156a2 2 0 002-2V22a2 2 0 00-2-2zm-2 76.92h-48.74V24H176v72.92zM75.64 63.44h47.62v73.5H75.64v-73.5zm47.62-4H75.64V24h47.62v35.44zm-47.62 81.5h47.62V176H75.64v-35.06zM71.64 24v36.9a1.762 1.762 0 00-.12.54c.009.185.05.368.12.54v34.94H24V24h47.64zM24 100.92h47.64v37.48a1.282 1.282 0 000 1.08V176H24v-75.08zM127.26 176v-75.08H176V176h-48.74z"
-        fill="#978673"
-        data-color="1"
-      />
-    </g>
-  </svg>
+const IconWall = () => (
+  <svg
+  data-bbox="20 20 160 160"
+  viewBox="20 20 160 160"
+  height={50}
+  width={50}
+  xmlns="http://www.w3.org/2000/svg"
+  data-type="color"
+  role="presentation"
+  aria-hidden="true"
+>
+  <g>
+    <path
+      d="M178 20H22a2 2 0 00-2 2v156a2 2 0 002 2h156a2 2 0 002-2V22a2 2 0 00-2-2zm-2 76.92h-48.74V24H176v72.92zM75.64 63.44h47.62v73.5H75.64v-73.5zm47.62-4H75.64V24h47.62v35.44zm-47.62 81.5h47.62V176H75.64v-35.06zM71.64 24v36.9a1.762 1.762 0 00-.12.54c.009.185.05.368.12.54v34.94H24V24h47.64zM24 100.92h47.64v37.48a1.282 1.282 0 000 1.08V176H24v-75.08zM127.26 176v-75.08H176V176h-48.74z"
+      fill="#978673"
+      data-color="1"
+    />
+  </g>
+</svg>
 );
 
-export default Icon;
+export default IconWall;

aa9ad949e942a33a506bf0d2e99abf59d3d89f7b
Juan Esteban Espinosa <juanestebanspinosa@gmail.com>
2024-08-25 22:17:05 -0500

    about us del home cambiando el padding

--- app/page.js
+++ app/page.js
@@ -47,7 +47,9 @@ export default function Home() {
         </div>
         <div className="absolute inset-0 flex flex-col items-center justify-center px-4 text-center text-white z-10">
           <img src="/img/logo.png" width="400px" />
-          <h1 className="text-xl lg:text-6xl font-bold mb-4">Magic Wood Work</h1>
+          <h1 className="text-xl lg:text-6xl font-bold mb-4">
+            Magic Wood Work
+          </h1>
           <h2 className="text-lg lg:text-2xl">
             Creating Your Dream Home with Our Expertise
           </h2>
@@ -85,8 +87,7 @@ export default function Home() {
               </p>
 
               <p className="lg:mb-6 font-thin lg:font-normal">
-                <span className="mr-2 ">•</span>Custom High Quality
-                Paint
+                <span className="mr-2 ">•</span>Custom High Quality Paint
               </p>
               <p className="lg:mb-12 mb-12 font-thin lg:font-normal">
                 <span className="mr-2 ">•</span>Installation
@@ -105,7 +106,7 @@ export default function Home() {
         </section>
 
         <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 lg:w-[90%] mx-auto">
-          <div className="border border-r-1 border-[#ac9172] flex flex-col justify-center items-center h-52" >
+          <div className="border border-r-1 border-[#ac9172] flex flex-col justify-center items-center h-52">
             <div className="flex flex-col items-center xy-12">
               <IconHouse />
               <span className="font-thin lg:font-light text-center mt-4 mx-12 lg:mx-0">
@@ -134,16 +135,26 @@ export default function Home() {
         </section>
 
         <section className="grid grid-cols-1 lg:grid-cols-2 mx-auto border border-r-1  border-[#ac9172] lg:w-[90%]">
-          <div className="border border-r-1 border-[#ac9172] p-6 lg:p-40">
+          <div className="border border-r-1 border-[#ac9172] p-6 lg:p-28">
             <h2 className="font-bold text-4xl">About Us</h2>
-            <h3 className="text-3xl my-4 font-light text-[#9a8b79]">Magic Wood Work</h3>
+            <h3 className="text-3xl my-4 font-light text-[#9a8b79] ">
+              Magic Wood Work
+            </h3>
             <p className="text-xl font-light text-justify">
-              At Magic Wood Work, we are dedicated to providing top-notch carpentry and woodworking services to enhance the appearance of your home or commercial space. With our many years of experience, we take pride in our ability to turn your dreams into a reality through our exceptional craftsmanship. We value building trust with our clients and always deliver the best quality work. Let us help make your space a place you love to be in.
+              At Magic Wood Work, we are dedicated to providing top-notch
+              carpentry and woodworking services to enhance the appearance of
+              your home or commercial space. With our many years of experience,
+              we take pride in our ability to turn your dreams into a reality
+              through our exceptional craftsmanship. We value building trust
+              with our clients and always deliver the best quality work. Let us
+              help make your space a place you love to be in.
             </p>
           </div>
-          <div className="border border-r-1 border-[#ac9172] h-[200px] lg:h-[670px] relative">
-            <img
+          <div className="border border-r-1 border-[#ac9172] relative">
+            <Image
               src="/img/workers1.webp"
+              layout="fill"
+              objectFit="cover"
               className="absolute object-cover inset-0 w-full h-full"
               alt="Carpentry workers in action"
             />
@@ -159,8 +170,6 @@ export default function Home() {
         <h1 className="text-center mt-32 text-2xl lg:text-3xl mb-12">
           Contact us
         </h1>
-
-        
       </div>
       <div className="mb-80 py-96"></div>
       <div className="mb-80 py-96"></div>
@@ -168,7 +177,6 @@ export default function Home() {
       <div className="py-24 lg:py-0"></div>
 
       <Contactanos className="mt-12" />
-
     </main>
   );
 }

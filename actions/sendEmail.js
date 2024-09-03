562b0791ed82e154da7ab13ea20f8f8e0425e767
Juan Esteban Espinosa <juanestebanspinosa@gmail.com>
2024-09-03 00:28:03 -0500

    contacto

--- actions/sendEmail.js
+++ actions/sendEmail.js
@@ -1,7 +1,9 @@
 'use server'
-import { NextResponse } from 'next/server';
 
-export async function sendEmail(formData) {
+import axios from 'axios';
+
+
+export const actionEmail = async (formData) => {
   const rawFormData = {
     firstName: formData.get('firstName'),
     lastName: formData.get('lastName'),
@@ -10,24 +12,12 @@ export async function sendEmail(formData) {
     address: formData.get('address'),
     message: formData.get('message'),
   };
+  console.log(JSON.stringify(rawFormData));
 
   try {
-    const response = await fetch('/api/email', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(rawFormData),
-    });
-
-    if (!response.ok) {
-      throw new Error('Failed to send email');
-    }
-
-    const result = await response.json();
-    return { success: true, message: result.message };
+    const resp = await axios.post(`${PUBLIC_NEXT_PUBLIC_API_URL}/api/email`, rawFormData);
+    return resp.data;
   } catch (error) {
-    console.error('Error sending email:', error);
-    return { success: false, message: 'Failed to send email' };
+    return { error: error.message };  // Return plain object
   }
-}   
+};

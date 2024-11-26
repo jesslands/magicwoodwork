cce2eda19ff011b17e3e5124202bc7c3880abdaa
Juan Esteban Espinosa <juanestebanspinosa@gmail.com>
2024-11-25 23:38:26 -0500

    sidemap.xml

---
+++ app/sitemap.xml/route.js
@@ -0,0 +1,42 @@
+import { MetadataRoute } from 'next'
+
+export function GET() {
+  return new Response(`<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>https://magicwoodwork.us</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+      </url>
+      <url>
+        <loc>https://magicwoodwork.us/services</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+      </url>
+      <url>
+        <loc>https://magicwoodwork.us/projects</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+      </url>
+      <url>
+        <loc>https://magicwoodwork.us/About</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.7</priority>
+      </url>
+      <url>
+        <loc>https://magicwoodwork.us/contact</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.6</priority>
+      </url>
+    </urlset>`, {
+    headers: {
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, max-age=3600, must-revalidate',
+    },
+  })
+}
